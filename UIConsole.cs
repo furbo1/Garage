30fca1f950af8b3ec1792e2eb78bc140c35d4986
furbo1 <al_razvan@yahoo.com>
2021-02-18 12:19:47 +0100

    Display all vehicles featrure added!

--- UIConsole.cs
+++ UIConsole.cs
@@ -20,7 +20,7 @@ namespace GarageApp
                 
                     Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
                         + "\n1. Instatiate a new garage with the desired number of parking lots"
-                        + "\n2. Display the number of available parking lots in the Garage"
+                        + "\n2. Display all the vehicles in the garage"
                         + "\n3. Add vehicles to garage"
                         + "\n4. Remove vehicles from garage"
                          + "\n5. Search vehicles in garage"
@@ -49,15 +49,20 @@ namespace GarageApp
 
                             break;
                         case '2':
-                        Utils.Print(str: $"The number of available parking lots is: { garage.ParkingLotsAvailable()} ");
+                        garage.DisplayAllVehicles();
+                       
+                        
                         break;
                         case '3':
                         Utils.Print("Please follow the below instructions to add vehicles to garage");
                         AddVehicleMenu();
                         break;
                         case '4':
-                           
-                            break;
+                        Utils.Print("Please follow the below instructions to remove vehicles from the garage");
+                        
+                        
+                        RemoveVehicleMenu();
+                        break;
                         case '5':
                            
                             break;
@@ -82,12 +87,14 @@ namespace GarageApp
 
             while (true)
             {
-                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4,5, 0) of your choice"
+                Console.Clear();
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4,5,6, 0) of your choice"
                     + "\n1. Press 1 to add a Car"
                     + "\n2. Press 2 to add a Boat"
                     + "\n3. Press 3 to add a Plane"
                     + "\n4. Press 4 to add a Bus"
-                     + "\n5. Press 5 to add a MotorCycle"
+                    + "\n5. Press 5 to add a MotorCycle"
+                    + "\n6. Return to previous menu"
                     + "\n0. Exit the application");
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
@@ -198,7 +205,153 @@ namespace GarageApp
                             $"Number of parking lots available is { garage.ParkingLotsAvailable()}");
 
                         break;
-                   
+
+                    case '6':
+                        Utils.Print($"Return to main menu: {6}");
+                        Menu();
+                        break;
+
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4, 5, 6)");
+                        break;
+                }
+            }
+
+        }
+
+        public void RemoveVehicleMenu()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 , 4, 5, 6, 0) of your choice"
+                    + "\n1. Press 1 to remove a Vehicle"
+                    //+ "\n2. Press 2 to remove a Boat"
+                    //+ "\n3. Press 3 to remove a Plane"
+                    //+ "\n4. Press 4 to remove a Bus"
+                    //+ "\n5. Press 5 to remove a MotorCycle"
+                    + "\n6. Return to previous menu"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Utils.ReadLine()[0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Utils.Print("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        Console.Clear();
+                        Utils.Print("Enter the follwing details of the Vehicle registration number to be removed");
+                        //Utils.Print("Car brand");
+                        //var make = Utils.ReadLine();
+                        //Utils.Print("Car Model");
+                        //var model = Utils.ReadLine();
+                        Utils.Print("Car Registration Number");
+                        var regNo = Utils.ReadLine();
+                        //Utils.Print("Car Color");
+                        //var color = Utils.ReadLine();
+
+
+                        //var car = new Car(make, model, regNo, color);
+                        
+                        garage.RemoveVehicleByRegNo(regNo);
+                       
+
+
+                        break;
+                    //case '2':
+                    //    Console.Clear();
+                    //    Utils.Print("Enter the follwing details of the Boat");
+                    //    Utils.Print("Boat brand");
+                    //    var bMake = Utils.ReadLine();
+                    //    Utils.Print("Boat Model");
+                    //    var bModel = Utils.ReadLine();
+                    //    Utils.Print("Boat Registration Number");
+                    //    var bRegNo = Utils.ReadLine();
+                    //    Utils.Print("Boat Color");
+                    //    var bColor = Utils.ReadLine();
+                    //    var boat = new Boat(bMake, bModel, bRegNo, bColor);
+                        
+                    //    garage.RemoveVehicle(boat);
+                    //    Utils.Print(str: $"Congratulations, you added a Boat {boat.Make}, model {boat.Type}, registration number {boat.RegNo} & color {boat.Color} to garage!\n " +
+                    //       $"Number of parking lots available is { garage.ParkingLotsAvailable()}");
+
+                    //    break;
+                    //case '3':
+                    //    Console.Clear();
+                    //    Utils.Print("Enter the follwing details of the Plane");
+                    //    Utils.Print("Plane brand");
+                    //    var pMake = Utils.ReadLine();
+                    //    Utils.Print("Plane Model");
+                    //    var pModel = Utils.ReadLine();
+                    //    Utils.Print("Plane Registration Number");
+                    //    var pRegNo = Utils.ReadLine();
+                    //    Utils.Print("Plane Color");
+                    //    var pColor = Utils.ReadLine();
+                    //    Utils.Print("Plane number of engines");
+                    //    var pEngines = Utils.ReadLine();
+
+                    //    var plane = new Plane(pMake, pModel, pRegNo, pColor, int.Parse(pEngines));
+                    //    garage.RemoveVehicle(plane);
+                    //    Utils.Print(str: $"Congratulations, you added a Plane {plane.Make}, model {plane.Type}, registration number {plane.RegNo} & color {plane.Color} to garage!\n " +
+                    //       $"Number of parking lots available is { garage.ParkingLotsAvailable()}");
+
+
+                    //    break;
+                    //case '4':
+                    //    Console.Clear();
+                    //    Utils.Print("Enter the follwing details of the Bus");
+                    //    Utils.Print("Bus brand");
+                    //    var busMake = Utils.ReadLine();
+                    //    Utils.Print("Bus Model");
+                    //    var busModel = Utils.ReadLine();
+                    //    Utils.Print("Bus Registration Number");
+                    //    var busRegNo = Utils.ReadLine();
+                    //    Utils.Print("Bus Color");
+                    //    var busColor = Utils.ReadLine();
+                    //    Utils.Print("Bus number of engines");
+                    //    var busSeats = Utils.ReadLine();
+
+                    //    var bus = new Bus(busMake, busModel, busRegNo, busColor, int.Parse(busSeats));
+                    //    garage.RemoveVehicle(bus);
+                    //    Utils.Print(str: $"Congratulations, you added a Bus {bus.Make}, model {bus.Type}, registration number {bus.RegNo} & color {bus.Color} to garage!\n " +
+                    //       $"Number of parking lots available is { garage.ParkingLotsAvailable()}");
+
+                    //    break;
+                    //case '5':
+                    //    Console.Clear();
+                    //    Utils.Print("Enter the follwing details of the MotorCycle");
+                    //    Utils.Print("Motorcycle brand");
+                    //    var mMake = Utils.ReadLine();
+                    //    Utils.Print("Motorcycle Model");
+                    //    var mModel = Utils.ReadLine();
+                    //    Utils.Print("Motorcycle Registration Number");
+                    //    var mRegNo = Utils.ReadLine();
+                    //    Utils.Print("Motorcycle Color");
+                    //    var mColor = Utils.ReadLine();
+
+
+                    //    var motorcycle = new Motorcycle(mMake, mModel, mRegNo, mColor);
+                    //    garage.RemoveVehicle(motorcycle);
+                    //    Utils.Print(str: $"Congratulations, you added a Motorcycle {motorcycle.Make}, model {motorcycle.Type}, registration number {motorcycle.RegNo} & color {motorcycle.Color} to garage!\n " +
+                    //        $"Number of parking lots available is { garage.ParkingLotsAvailable()}");
+
+                    //    break;
+
+                    case '6':
+                        Utils.Print($"Return to main menu: {6}");
+                        Menu();
+                        break;
+
                     case '0':
                         Environment.Exit(0);
                         break;
@@ -209,6 +362,6 @@ namespace GarageApp
             }
 
         }
-      
+
     }
 }
